0a3726a921af7f9c7b342397654271d17a3604a4
vintagedaddyo <vintagedaddyo@gmail.com>
2022-05-23 09:36:19 -0700

    ...

--- Upload/admin/styles/sharepoint8/style.php
+++ Upload/admin/styles/sharepoint8/style.php
@@ -31,10 +31,10 @@ class Page extends DefaultPage
 		$trail = "";
 		foreach($this->breadcrumb_trail as $key => $crumb)
 		{
-			if($this->breadcrumb_trail[$key+1])
+			if(isset($this->breadcrumb_trail[$key+1]))
 			{
 				$trail .= "<a href=\"".$crumb['url']."\">".$crumb['name']."</a>";
-				if($this->breadcrumb_trail[$key+2])
+				if(isset($this->breadcrumb_trail[$key+2]))
 				{
 					$trail .= " &raquo; ";
 				}
@@ -64,11 +64,13 @@ class Page extends DefaultPage
 					continue;
 				}
 				$class = '';
-				if($tab['link_target'])
+				if(isset($tab['link_target']))
 				{
 					$target = " target=\"{$tab['link_target']}\"";
 				}
 				if($first) $class .= " first";
+                // 74 sort warning
+                $target = isset($target) ? $target : ''; 
 				echo "\t\t<li class=\"{$class}\"><a href=\"{$tab['link']}\"{$target}>{$tab['title']}</a></li>\n";
 				$first = false;
 				$target = '';
@@ -129,6 +131,8 @@ class Page extends DefaultPage
 		echo "	<link rel=\"stylesheet\" href=\"styles/".$this->style."/main.css?ver=1813\" type=\"text/css\" />\n";
 		echo "	<link rel=\"stylesheet\" href=\"styles/".$this->style."/modal.css?ver=1813\" type=\"text/css\" />\n";
 
+        // Add CP Style Specific Favicon
+		echo " <link rel=\"Shortcut icon\" href=\"styles/".$this->style."/images/favicon.ico\">\n";		
 		// Load stylesheet for this module if it has one
 		if(file_exists(MYBB_ADMIN_DIR."styles/{$this->style}/{$this->active_module}.css"))
 		{
@@ -153,6 +157,18 @@ class Page extends DefaultPage
 				"	document.write('<style type=\"text/css\">.popup_button { display: inline; } .popup_menu { display: none; }<\/style>');\n".
                 "//]]>\n".
                 "</script>\n";
+
+        // ADD Progressbar
+        echo " <link rel=\"stylesheet\" href=\"styles/".$this->style."/inc/progbar/nprogress.css\"/>\n";
+        echo" <script type=\"text/javascript\" src=\"styles/".$this->style."/inc/progbar/nprogress.js\"></script>\n";
+
+        echo "<script>
+	        NProgress.configure({ showSpinner: false });
+		    $(document).ready(function() {
+			NProgress.start();
+			NProgress.done();
+		    }); 
+            </script>\n";                 
 
 		echo "	<script type=\"text/javascript\">
 //<![CDATA[
